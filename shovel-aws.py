b0f344b58b3680e1260719469e48f49d6f951236
scriptrdotio <git@apstrata.com>
2018-01-05 19:38:16 +0200

    update code for otaa
    otaa join has no need for lora packet decode

--- shovel-aws.py
+++ shovel-aws.py
@@ -3,8 +3,8 @@ import paho.mqtt.client as mqtt
 import json
 #aws configuration
 awsClient = AWSIoTMQTTClient("multitech-gateway")
-awsClient.configureEndpoint("axxxxxxxxx.iot.us-west-2.amazonaws.com", 8883)
-awsClient.configureCredentials("/home/root/aws/root-CA.crt", "/home/root/aws/conduit.private.key", "/home/root/aws/conduit.cert.pem")
+awsClient.configureEndpoint("a3m9jbp3s169k0.iot.us-east-1.amazonaws.com", 8883)
+awsClient.configureCredentials("/home/walid/loramote/root-CA.crt", "/home/walid/loramote/conduit.private.key", "/home/walid/loramote/conduit.cert.pem")
 awsClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 awsClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 awsClient.configureConnectDisconnectTimeout(10)  # 10 sec
@@ -13,21 +13,11 @@ awsClient.configureMQTTOperationTimeout(5)  # 5 sec
 def on_message(client, userdata, msg):
         print(msg.topic+" "+str(msg.payload))
         #push message to aws
-        #create a new message which includes the topic and a decrypted version of the payload
-        message = {}
-        try:
-                originalPayload = json.loads(msg.payload)
-                if 'data' in originalPayload:#this is a data packet, decode lora encryption
-                        message = {}
-                        message['original'] = msg.payload
-                else:#this isn't a data packet, just send it
-                        message = msg.payload
-        except:
-                print 'not a valid json'
-                print msg.payload
-                message = msg.payload
-        #in all cases, add original topic to message
-        message['topic'] = msg.topic
+        #create a new message which includes the topic and a the payload
+        message = {
+		'payload': msg.payload,
+		'topic': msg.topic
+	}
         awsClient.publish(msg.topic, json.dumps(message), 0)
 
 subscribed = False
