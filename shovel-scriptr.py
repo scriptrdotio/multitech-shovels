e1cc8b05ab208046f5ce0f01bc7b6e089959988a
scriptrdotio <git@apstrata.com>
2018-01-05 19:14:54 +0200

    updated shovel code to exclude "original" data obj
    and use the legacy mqtt bridge

--- shovel-scriptr.py
+++ shovel-scriptr.py
@@ -3,16 +3,18 @@ import json
 localClient = mqtt.Client()
 scriptrClient = mqtt.Client()
 def on_message(client, userdata, msg):
-scriptrMessage = { #message in the format scriptr.io expects
-"method":"Publish",
-"params":{
-"apsdb.channel": "multitech" #channel on which to publish the message
-"apsdb.message": JSON.dumps({"payload": msg.payload, "topic":msg.topic})
-}
-}
-scriptrClient.publish("VzE3XXXXXXXXX", json.dumps(scriptrMessage)) # Use your scriptr.io Access Token as shown in the above figure
+        print "received message" + msg.topic + " " + msg.payload
+        scriptrMessage = {
+                'method':'Publish',
+                'params':{
+                        'apsdb.channel': "multitech", #channel on which to publish the message
+                        'apsdb.message': json.dumps({'payload': msg.payload, 'topic':msg.topic})
+                }
+        }
+        scriptrClient.publish("xxxxxxxx==", json.dumps(scriptrMessage)) # Use your scriptr.io Access Token as shown in the above figure
+        print "published message" + json.dumps(scriptrMessage)
 localClient.connect("localhost", 1883, 60) #local broker on gateway
-scriptrClient.connect("mqtt.scriptrapps.io", 1883, 60) #scriptr.io mqtt endpoint
+scriptrClient.connect("bridges.scriptr.io", 1883, 60) #scriptr.io mqtt endpoint
 localClient.on_message = on_message
 localClient.subscribe("#", 0)
 localClient.loop_forever() #forever loop since the on_message is async and we need to leave script running
